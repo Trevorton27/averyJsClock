34f31fbbdb208ecda26da84613eff8cfa76b7f7f
Trevor Mearns <spiral272@gmail.com>
2021-01-08 14:26:57 +0900

    final refactor ready for review

--- script.js
+++ script.js
@@ -1,73 +1,46 @@
 function updateClock() {
-  let now = new Date();
-  let day = now.getDay();
-  let month = now.getMonth();
-  let date = now.getDate();
-  let year = now.getFullYear();
-  let hour = now.getHours();
-  let minute = now.getMinutes();
-  let second = now.getSeconds();
-  let period = 'AM';
+  const now = new Date();
 
-  if (hour == 0) {
-    hour = 12;
-  }
-  if (hour > 12) {
-    hour = hour - 12;
-    period = 'PM';
-  }
+  const hour = document.getElementById('hour');
+  const minute = document.getElementById('minutes');
+  const second = document.getElementById('seconds');
+  const period = document.getElementById('period');
 
-  Number.prototype.pad = function (digits) {
-    for (var num = this.toString(); num.length < digits; num = 0 + num);
-    console.log('digits: ', digits);
-    return num;
-  };
-  const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
-  ];
-  const week = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday'
-  ];
-  const ids = [
-    'dayname',
-    'month',
-    'daynum',
-    'year',
-    'hour',
-    'minutes',
-    'seconds',
-    'period'
-  ];
-  const values = [
-    week[day],
-    months[month],
-    date.pad(2),
-    year.pad(2),
-    hour.pad(2),
-    minute.pad(2),
-    second.pad(2),
-    period
-  ];
-  for (var i = 0; i < ids.length; i++)
-    document.getElementById(ids[i]).firstChild.nodeValue = values[i];
+  let getHour = now.getHours();
+  let getMinute = now.getMinutes();
+  let getSecond = now.getSeconds();
+  let getPeriod = getHour > 12 ? 'PM' : 'AM';
+
+  hour.textContent = getHour > 12 ? getHour - 12 : getHour;
+  minute.textContent = getMinute < 10 ? `0${getMinute}` : getMinute;
+  second.textContent = getSecond < 10 ? `0${getSecond}` : getSecond;
+  period.textContent = getPeriod;
+}
+
+function updateDate() {
+  const now = new Date();
+
+  const year = document.getElementById('year');
+  const month = document.getElementById('month');
+  const day = document.getElementById('dayname');
+  const date = document.getElementById('daynum');
+
+  const getYear = now.getFullYear();
+  const getMonth = now.toLocaleString('default', { month: 'long' });
+  const getDay = now.toLocaleString('default', { weekday: 'long' });
+  const getDate = now.getDate();
+
+  year.textContent = getYear;
+  month.textContent = getMonth;
+  day.textContent = getDay;
+  date.textContent = getDate < 10 ? `0${getDate}` : getDate;
+}
+
+function runClock() {
+  updateClock();
+  updateDate();
 }
 
 updateClock();
-setInterval(updateClock, 1000);
+
+window.setInterval(runClock, 1000);
